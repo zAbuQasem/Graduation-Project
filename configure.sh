156359a044e553d0d7cf2d93c13bce0e24b0c0b3
zAbuQasem <74984708+zAbuQasem@users.noreply.github.com>
2022-02-16 16:06:30 +0200

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -10,12 +10,15 @@ then
 	apt install -y python3-pip docker vim
 
 	echo -e "[-] Setting SETUID capability for python3\n"
-	setcap "cap_setuid+ep" /usr/bin/python3.8
-
+	PYTHON=$(which python3)
+	setcap "cap_setuid+ep" $PYTHON
+	
+	# Add the changes to a file and then remove this block
 	echo -e "[-] Configuring sudoers file\n"
 	echo -e '\n# BAU project :'>> /etc/sudoers
 	echo '%Administrators	ALL=(ALL:ALL) ALL' >> /etc/sudoers
-	echo 'Khaled ALL=(root) NOPASSWD: /usr/bin/vi' >> /etc/sudoers
+	echo 'Khaled ALL=(root) NOPASSWD: /usr/bin/vim' >> /etc/sudoers
+	#echo 'Nabil ALL=(root) NOPASSWD: /usr/bin/docker' >> /etc/sudoers
 
 	echo -e "Creating Users and configuring them\n"
 	chmod +x /scripts/config_users.py 
