e568535bf4716f26a5fe63d8fd34a59003b5b387
zAbuQasem <74984708+zAbuQasem@users.noreply.github.com>
2022-02-16 16:12:07 +0200

    Create Configuration.sh

---
+++ Server-Conf/Configuration.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+<<END
+* This file will contain required configurations to setup the whole server...
+END
+
+if [[ "$UID" -eq "0" ]];
+then
+	echo -e "[-] Installing dependencies\n"
+	apt install -y python3-pip docker vim
+
+	echo -e "[-] Setting SETUID capability for python3\n"
+	PYTHON=$(which python3)
+	setcap "cap_setuid+ep" $PYTHON
+	
+	# Add the changes to a file and then remove this block
+	echo -e "[-] Configuring sudoers file\n"
+	echo -e '\n# BAU project :'>> /etc/sudoers
+	echo '%Administrators	ALL=(ALL:ALL) ALL' >> /etc/sudoers
+	echo 'Khaled ALL=(root) NOPASSWD: /usr/bin/vim' >> /etc/sudoers
+	#echo 'Nabil ALL=(root) NOPASSWD: /usr/bin/docker' >> /etc/sudoers
+
+	echo -e "Creating Users and configuring them\n"
+	chmod +x /scripts/config_users.py 
+	python3 /scripts/config_users.py
+else
+	echo "[!] Root privileges required"
+fi
