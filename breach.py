aeb7e17e6b8d307fb94a362f6b5779507f5ff36c
zAbuQasem <74984708+zAbuQasem@users.noreply.github.com>
2022-02-19 04:45:02 +0200

    Delete breach.py

--- breach.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-
-PassPolicy = "_threatints2022!"
-with open("names.txt", "r") as f:
-	for user in f.readlines():
-		user = user.strip()
-		creds = user+": "+user + PassPolicy
-		print(creds)
